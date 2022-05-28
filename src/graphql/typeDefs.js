43b85050c4dd7b920a2403d3ec0f630d2cff2701
Shane Goonasekera <theshanukg@gmail.com>
2022-05-27 21:56:50 -0600

    fix merge issues

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,14 +1,11 @@
 import { gql } from 'apollo-server-express';
-import { User } from './User';
 import { Location } from './Location';
 import { SpotOfInterest } from './SpotOfInterest';
 import { Trip } from './Trip';
 import { Place } from './Place';
 import { DayPlan } from './DayPlan';
 
-
 const typeDefs = gql`
-  ${User.types}
   ${Location.types}
   ${SpotOfInterest.types}
   ${Trip.types}
@@ -16,12 +13,11 @@ const typeDefs = gql`
   ${DayPlan.types}
   
   type Query {
-    ${User.queries}
     ${Location.queries}
     ${SpotOfInterest.queries}
-  
+  }
+
   type Mutation {
-    ${User.mutations}
     ${Location.mutations}
     ${SpotOfInterest.mutations}
     ${Trip.mutations}
