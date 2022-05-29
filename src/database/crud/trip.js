3bbb2d6d2fa5c014f73a62e3cc2cc9647a69acea
neilZon <neilcviloria@gmail.com>
2022-05-28 21:56:58 -0600

    fix oopsy

--- src/database/crud/trip.js
+++ src/database/crud/trip.js
@@ -13,48 +13,48 @@ export const createTrip = async (trip) => {
 
   let newTrip;
   try {
-    // rip if you gotta read this shit
-    // extract spotsOfInterest, dayPlan and places
-    const extractedSpotsOfInterests = [];
-    extractTripValues(trip, 'spotsOfInterest', extractedSpotsOfInterests);
+    //   // rip if you gotta read this shit
+    //   // extract spotsOfInterest, dayPlan and places
+    //   const extractedSpotsOfInterests = [];
+    //   extractTripValues(trip, 'spotsOfInterest', extractedSpotsOfInterests);
 
-    const extractedDayPlan = [];
-    extractTripValues(trip, 'dayPlan', extractedDayPlan);
+    //   const extractedDayPlan = [];
+    //   extractTripValues(trip, 'dayPlan', extractedDayPlan);
 
-    const extractedPlaces = [];
-    extractTripValues(trip, 'places', extractedPlaces);
+    //   const extractedPlaces = [];
+    //   extractTripValues(trip, 'places', extractedPlaces);
 
-    // save spots of interest
-    const createdSpotsOfInterest = await SpotOfInterest.insertMany(
-      extractedSpotsOfInterests,
-      {
-        session,
-      }
-    );
+    //   // save spots of interest
+    //   const createdSpotsOfInterest = await SpotOfInterest.insertMany(
+    //     extractedSpotsOfInterests,
+    //     {
+    //       session,
+    //     }
+    //   );
 
-    // distribute the ObjectId's of created spots of interest to day plans
-    const initialValue = 0;
-    extractedDayPlan.reduce((prevVal, curVal) => {
-      const dayPlanSpots = createdSpotsOfInterest.slice(
-        prevVal,
-        prevVal + curVal.spotsOfInterest.length
-      );
-      curVal.spotsOfInterest = dayPlanSpots.map(
-        (dayPlanSpot) => dayPlanSpot._id
-      );
-      return prevVal + curVal.spotsOfInterest.length;
-    }, initialValue);
+    //   // distribute the ObjectId's of created spots of interest to day plans
+    //   const initialValue = 0;
+    //   extractedDayPlan.reduce((prevVal, curVal) => {
+    //     const dayPlanSpots = createdSpotsOfInterest.slice(
+    //       prevVal,
+    //       prevVal + curVal.spotsOfInterest.length
+    //     );
+    //     curVal.spotsOfInterest = dayPlanSpots.map(
+    //       (dayPlanSpot) => dayPlanSpot._id
+    //     );
+    //     return prevVal + curVal.spotsOfInterest.length;
+    //   }, initialValue);
 
-    // save day plans
-    const createdDayPlans = await DayPlan.insertMany(extractedDayPlan, {
-      session,
-    });
+    //   // save day plans
+    //   const createdDayPlans = await DayPlan.insertMany(extractedDayPlan, {
+    //     session,
+    //   });
 
-    // distribute day plan Id's to each day plan
-    extractedPlaces.forEach((place, i) => (place.dayPlan = createdDayPlans[i]));
+    //   // distribute day plan Id's to each day plan
+    //   extractedPlaces.forEach((place, i) => (place.dayPlan = createdDayPlans[i]));
 
     // save places
-    const createdPlaces = await Place.insertMany(extractedPlaces, { session });
+    const createdPlaces = await Place.create(places, { session });
 
     // map to array of just the place id's
     const placeIds = createdPlaces.map((place) => place._id);
@@ -72,11 +72,13 @@ export const createTrip = async (trip) => {
       ],
       { session }
     );
+    newTrip = newTrip[0];
     await session.commitTransaction();
   } catch (err) {
     console.error(err);
     await session.abortTransaction();
   } finally {
     await session.endSession();
+    return newTrip;
   }
 };
