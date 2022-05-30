9c66f800757f5e5cb3e2a58b4b9ec5bef39e26ac
Shane Goonasekera <theshanukg@gmail.com>
2022-05-29 19:15:09 -0600

    some model changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './graphql';
 
-// import swaggerJsdoc from 'swagger-jsdoc';
-// import swaggerUi from 'swagger-ui-express';
-// import { swaggerOptions } from './config/docs';
-
 import dotenv from 'dotenv';
 dotenv.config();
 
-import * as routes from './routes';
+// import * as routes from './routes';
 import FoursquareAPI from './graphql/Datasources/foursquare';
 // import { validateToken } from './middleware/auth';
 
@@ -46,29 +42,22 @@ const startServer = async () => {
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
 
-  //docs
-  // app.use(
-  //   '/docs',
-  //   swaggerUi.serve,
-  //   swaggerUi.setup(swaggerJsdoc(swaggerOptions))
-  // );
-
   // middleware
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  // app.use(express.json());
+  // app.use(express.urlencoded({ extended: false }));
 
   // routes
-  app.use('/auth', routes.authRoutes);
-  app.use('/newsletter', routes.newsletterRoutes);
-  app.use('/flights', routes.flightRoutes);
-  app.use('/lodging', routes.lodgingRoutes);
+  // app.use('/auth', routes.authRoutes);
+  // app.use('/newsletter', routes.newsletterRoutes);
+  // app.use('/flights', routes.flightRoutes);
+  // app.use('/lodging', routes.lodgingRoutes);
 
   // auth middleware
   // app.use(validateToken);
 
   // authenticated routes
-  app.use('/users', routes.userRoutes);
-  app.use('/trips', routes.tripRoutes);
+  // app.use('/users', routes.userRoutes);
+  // app.use('/trips', routes.tripRoutes);
 
   // general error handler
   // NOTE: Must be last middleware
