e4e4488b48c7eb6e8ca5c2d8e2b8317b9441effe
neilZon <neilcviloria@gmail.com>
2022-06-22 23:27:56 -0600

    remove connectdb

--- server.ts
+++ server.ts
@@ -1,8 +1,4 @@
 import startServer from './src';
-import connectDb from './src/graphql/datasources/database';
-
-//connect mongodb
-connectDb();
 
 //start server
 startServer();
--- src/graphql/datasources/database/index.ts
+++ src/graphql/datasources/database/index.ts
@@ -1,26 +1,3 @@
-import mongoose from 'mongoose';
+import { PrismaClient } from '@prisma/client';
 
-import dotenv from 'dotenv';
-dotenv.config();
-
-const connectDb = () => {
-  mongoose.connect(
-    process.env.MONGO_DATABASE_CONNECTION_STRING as string,
-    () => {
-      console.log('Main DB Connected.');
-    }
-  );
-
-  // TODO: need to find a better way to register models for .populate() nested ObjectId's
-  // https://intellipaat.com/community/34120/mongoose-schema-hasnt-been-registered-for-model
-  require('./models/spotOfInterest');
-  require('./models/trip');
-  require('./models/dayPlan');
-  require('./models/place');
-  require('./models/user');
-  require('./models/session');
-
-  mongoose.connection.on('error', (error) => console.error(error));
-};
-
-export default connectDb;
+export const prismaClient = new PrismaClient();
