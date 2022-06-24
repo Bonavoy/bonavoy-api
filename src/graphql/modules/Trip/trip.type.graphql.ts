023586ae6519ed6891cc5d989af1aaad1629d8f9
neilZon <neilcviloria@gmail.com>
2022-06-23 19:53:40 -0600

    graphql and db trip functionality

--- src/graphql/modules/Trip/trip.type.graphql.ts
+++ src/graphql/modules/Trip/trip.type.graphql.ts
@@ -17,41 +17,12 @@ export default gql`
   input TripInput {
     name: String
     isPublic: Boolean
-    places: [PlaceInput]
-    author: String
-  }
-
-  # todo: need to move place module
-  input PlaceInput {
-    mapbox_id: String
-    name: String
-    from: String
-    to: String
-    duration: Int
-    dayPlan: DayPlanInput
-  }
-
-  input DayPlanInput {
-    date: String
-    spotsOfInterest: [SpotOfInterestInput]
   }
 
   #types
   type Trip {
     id: ID
     name: String
-    author: String
     isPublic: Boolean
-    places: [Place]
-  }
-
-  type Place {
-    id: ID
-    name: String
-    mapbox_id: String
-    from: String
-    to: String
-    duration: Int
-    dayPlan: [DayPlan]
   }
 `;
