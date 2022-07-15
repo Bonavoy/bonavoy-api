6ea26b6a544dc4e5c95eef0a9a8723f2130164f2
Shane Goonasekera <theshanukg@gmail.com>
2022-07-14 22:52:45 -0600

    updated couple rules

--- src/graphql/permissions/rules.ts
+++ src/graphql/permissions/rules.ts
@@ -4,7 +4,12 @@ import { Context } from '../../types/auth'
 
 export const isAuthenticated = rule({ cache: 'contextual' })(async (_parent: unknown, _args: unknown, ctx: Context) => {
   //basically see if ACESS TOKEN has a user id once verified in context of grapgql
-  return !!ctx.auth.sub
+
+  //checks if there is a valid auth token
+  if (!!ctx.auth.sub) return true
+  //if no auth token, check if we have a valid refresh token
+  else if (!!!ctx.auth.sub && !!ctx.auth.refresh.sub) return 'Token expired!'
+  return false
 })
 
 export const isSession = rule({ cache: 'contextual' })(async (_parent: unknown, _args: unknown, ctx: Context) => {
