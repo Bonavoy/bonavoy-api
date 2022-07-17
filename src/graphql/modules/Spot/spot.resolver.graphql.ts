dd06a19f4299d8bc1b1ff53e45fa5ca9dc49e43c
neilZon <neilcviloria@gmail.com>
2022-07-17 16:32:22 -0600

    find venue recommendations

--- src/graphql/modules/Spot/spot.resolver.graphql.ts
+++ src/graphql/modules/Spot/spot.resolver.graphql.ts
@@ -5,27 +5,6 @@ import { BonavoyDataSources } from '../../datasources'
 
 // TODO: WRITE TYPES
 export default {
-  Query: {
-    getSpotRecommendations: async (
-      _: unknown,
-      args: { input: { coords: { lat: number; lng: number }; limit: number } },
-      {
-        ctx,
-        req,
-        res,
-        dataSources,
-      }: { ctx: TokenDecoded; req: Request; res: Response; dataSources: BonavoyDataSources },
-    ) => {
-      //WRITE TYPES
-      const { input } = args
-      const options = {
-        coords: input.coords,
-        limit: input.limit,
-      }
-      const spotRecommendations = await dataSources.foursquareAPI.getSpots(options)
-      return spotRecommendations
-    },
-  },
   Mutation: {
     addSpotToDayPlan: async (
       _: unknown,
--- src/graphql/permissions/index.ts
+++ src/graphql/permissions/index.ts
@@ -3,12 +3,15 @@ import { shield } from 'graphql-shield'
 //rules
 import { isAuthenticated, isSession } from './rules'
 
-export default shield({
-  Query: {
-    user: isAuthenticated,
+export default shield(
+  {
+    Query: {
+      user: isAuthenticated,
+    },
+    Mutation: {
+      token: isSession,
+      createTrip: isAuthenticated,
+    },
   },
-  Mutation: {
-    token: isSession,
-    createTrip: isAuthenticated,
-  },
-})
+  { debug: true },
+)
