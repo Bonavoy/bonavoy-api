0469f08fb3d805febcfedd55b99093c0c3cdd9fe
neilZon <neilcviloria@gmail.com>
2022-08-21 21:21:31 -0600

    filtering

--- src/graphql/modules/SpotSuggestion/spotSuggestion.resolver.graphql.ts
+++ src/graphql/modules/SpotSuggestion/spotSuggestion.resolver.graphql.ts
@@ -10,6 +10,7 @@ export default {
       const options = {
         coords: input.coords,
         pageSize: input.pageSize ?? 16,
+        filters: input.filters ?? [],
         ...(input.cursor && { cursor: input.cursor }),
       }
       const spotSuggestions = await ctx.dataSources.foursquareAPI.getSpotSuggestions(options)
