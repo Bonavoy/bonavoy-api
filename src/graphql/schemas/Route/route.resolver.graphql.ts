900315f77ae170c0b6640ec05413544a2855b53c
Neilzon <neilcviloria@gmail.com>
2023-05-28 18:24:31 -0600

    transportation duration

--- src/graphql/schemas/Route/route.resolver.graphql.ts
+++ src/graphql/schemas/Route/route.resolver.graphql.ts
@@ -1,19 +1,24 @@
 // types
 import { Context } from '@bonavoy/types/auth'
 import { Resolvers } from '@bonavoy/generated/graphql'
-import { GraphQLError } from 'graphql'
 
 // TODO: WRITE TYPES
 export const resolvers: Resolvers = {
   Query: {
-    routeSegments: async (_parent, { segmentWaypoints }, ctx: Context) => {
-      const routeSegmentPromises = segmentWaypoints.map((waypoint) => ctx.dataSources.mapboxAPI.getRoute(waypoint))
+    routeLegs: async (_parent, { routeWaypoints }, ctx: Context) => {
+      const routeSegmentPromises = routeWaypoints.map((waypoint) => ctx.dataSources.mapboxAPI.getRoute(waypoint))
       const routeSegmentResults = await Promise.all(routeSegmentPromises)
       return routeSegmentResults.map((routeSegment) => {
         if (routeSegment.code === 'NoRoute' || routeSegment.code === 'NoSegment' || routeSegment.routes.length === 0) {
-          return []
+          return {
+            segments: [],
+            duration: 0,
+          }
         }
-        return routeSegment.routes[0].geometry.coordinates
+        return {
+          segments: routeSegment.routes[0].geometry.coordinates,
+          duration: routeSegment.routes[0].duration,
+        }
       })
     },
   },
