7693cd314b6b653ac76f615c5929b52717727960
Neilzon <neilcviloria@gmail.com>
2023-06-23 20:21:40 -0600

    add country to location resolver

--- src/graphql/schemas/Location/location.type.graphql.ts
+++ src/graphql/schemas/Location/location.type.graphql.ts
@@ -4,26 +4,20 @@ export default gql`
   #queries
   type Query {
     getLocationSuggestions(
-      query: String! # types: [String!]! # country: [String!]!
-    ): # proximity: [String!]!
-    [LocationSuggestion!]!
+      query: String! # types: [String!]! # country: [String!]! # proximity: [String!]!
+    ): [LocationSuggestion!]!
   }
-
-  # mutations
-  # type Mutation {}
 
   #types
   type LocationSuggestion {
     name: String!
     text: String!
     center: Coords!
-    # context: [LocationContext!]!
+    country: Country!
   }
 
-  type LocationContext {
-    id: String!
-    short_code: String
-    wikidata: String
+  type Country {
+    shortCode: String!
     text: String!
   }
 `
